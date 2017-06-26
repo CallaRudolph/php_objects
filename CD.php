6be823b5513e30681c462276b00de1e34cb81d01
Calla Rudolph <callarudolph@gmail.com>
2017-06-25 20:41:04 -0700

    encapsulation exercise

--- CD.php
+++ CD.php
@@ -5,7 +5,7 @@
         public $title;
         public $artist;
         public $cover_art;
-        public $price;
+        private $price;
 
         function __construct($album_name, $band_name, $image_path, $album_price = 10.99)
         {
@@ -14,12 +14,28 @@
             $this->cover_art = $image_path;
             $this->price = $album_price;
         }
+
+        function setPrice($new_price)
+        {
+            $float_price = (float) $new_price;
+            if ($float_price != 0) {
+              $formatted_price = number_format($float_price, 2);
+              $this->price = $formatted_price;
+            }
+        }
+
+        function getPrice()
+        {
+            return $this->price;
+        }
     }
 
     $first_cd = new CD("Master of Reality", "Black Sabbath", "img/reality.jpeg");
     $second_cd = new CD("Electric Ladyland", "Jimi Hendrix", "img/ladyland.jpeg");
     $third_cd = new CD("Nevermind", "Nirvana", "img/nevermind.jpeg");
     $fourth_cd = new CD("I don't get it", "Pork Lion", "img/porklion.jpeg", 49.99);
+
+    $fourth_cd->setPrice("1.3925");
 
     $cds = array($first_cd, $second_cd, $third_cd, $fourth_cd);
 ?>
@@ -33,6 +49,7 @@
     <div class="container">
         <?php
             foreach ($cds as $album) {
+                $cd_price = $album->getPrice();
                 echo "<div class='row'>
                     <div class='col-md-6'>
                         <img src='$album->cover_art'>
@@ -40,7 +57,7 @@
                     <div class='col-md-6'>
                         <p>$album->title</p>
                         <p>By $album->artist</p>
-                        <p>$$album->price</p>
+                        <p>$$cd->price</p>
                     </div>
                 </div>
                 ";
